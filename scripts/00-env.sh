04c26ab10bc61fcda59e2c07b9c54a684b413ecc
Richard Croft <rcroft@vmware.com>
2024-10-30 07:30:57 +0000

    Update 00-env.sh for internal registry and disable GitHub Actions

--- scripts/00-env.sh
+++ scripts/00-env.sh
@@ -19,12 +19,12 @@ TAG=dev
 VERSION=0.0.1
 
 # project info
-PROJECT="sydney/containers/$IMAGE"
+PROJECT="grumpydumpty/$IMAGE"
 
 # maintainer info
 MAIN_USER="Richard Croft"
-MAIN_EMAIL="rcroft@vmware.com"
-MAIN_URL="https://gitlab.eng.vmware.com/rcroft"
+MAIN_EMAIL="rcroft@omnissa.com"
+MAIN_URL="https://github.com/grumpydumpty"
 
 # below needs to match "ARG LABEL_PREFIX=" in Dockerfile
 LABEL_PREFIX=com.vmware.eocto
@@ -36,5 +36,6 @@ DOMAIN="lab.net"
 WORKDIR="/workspace"
 
 # repository to push image to
-# REPO=harbor.sydeng.vmware.com/rcroft
-REPO=ghcr.io/grumpydumpty/${IMAGE}
+#REPO=harbor.sydeng.vmware.com/rcroft
+#REPO=ghcr.io/grumpydumpty/${IMAGE}
+REPO=artifactory.build.omnissa.com/uem-platform-eng-docker
